16b2228472d9bd3e832e85a04a379981a393019b
Daniel Maslowski <info@orangecms.org>
2022-07-29 19:38:56 +0200

    rework for arbitrary 2M payload
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- test-d1-flash-bt0/src/main.rs
+++ test-d1-flash-bt0/src/main.rs
@@ -74,16 +74,8 @@ pub struct HeadData {
 
 const STAMP_CHECKSUM: u32 = 0x5F0A6C39;
 
-const ORE_ADDR: usize = RAM_BASE;
-const ORE_SIZE: usize = 0x1 << 16; // 64K
-const DTF_SIZE: usize = 0x1 << 16; // 64K
-const LIN_ADDR: usize = ORE_ADDR + 0x20_0000; // Linux expects to be at 0x4020_0000
-const LIN_SIZE: usize = 0x00ee_0000;
-const DTB_ADDR: usize = LIN_ADDR + 0x100_0000; // dtb must be 2MB aligned and behind Linux
-const DTB_SIZE: usize = 0x0000_e000;
-const DTB_END1: usize = DTB_ADDR + 0x6000;
-const DTB_END2: usize = DTB_ADDR + 0x6010;
-const DTB_END3: usize = DTB_ADDR + 0x6020;
+const PAYLOAD_ADDR: usize = RAM_BASE;
+const PAYLOAD_SIZE: usize = 0x20_0000; // 2 megs, plenty for a simple payload :)
 
 // clobber used by KEEP(*(.head.data)) in link script
 #[link_section = ".head.data"]
@@ -144,19 +136,6 @@ pub unsafe extern "C" fn start() -> ! {
     )
 }
 
-// map well-known addresses to expected values
-fn addr_to_exp(a: usize) -> u32 {
-    match a {
-        ORE_ADDR => 0x30401073, // oreboot (mie)
-        LIN_ADDR => 0x0000a0d1, // Linux
-        DTB_ADDR => 0xedfe0dd0, // dtb (d00dfeed)
-        DTB_END1 => 0x646e2c76, // dtb near end
-        DTB_END2 => 0x6d6d0079, // dtb near end
-        DTB_END3 => 0x756f6474, // dtb near end
-        _ => 0x0,
-    }
-}
-
 /// helper for debugging
 fn dump(addr: usize) {
     let val = unsafe { read_volatile(addr as *mut u32) };
@@ -169,15 +148,6 @@ fn check_val(addr: usize, val: u32) {
     if rval != val {
         println!("MISMATCH {:x} r{:08x} :: {:08x}", addr, rval, val);
     }
-
-    match addr {
-        ORE_ADDR | LIN_ADDR | DTB_ADDR | DTB_END1 | DTB_END2 | DTB_END3 => {
-            let exp = addr_to_exp(addr);
-            let ok = if rval == exp as u32 { "OK" } else { "NO" };
-            println!("{:08x}@{:08x} - {:08x}? {}", rval, addr, exp, ok);
-        }
-        _ => {}
-    };
 }
 
 #[cfg(feature = "nor")]
@@ -196,7 +166,8 @@ fn load(
     >,
 ) {
     let chunks = 16;
-    for i in 0..size / chunks {
+    let sz = size >> 2;
+    for i in 0..sz / chunks {
         let off = skip + i * 4 * chunks;
         let buf = f.copy_into([(off >> 16) as u8, (off >> 8) as u8, off as u8]);
 
@@ -300,22 +271,8 @@ extern "C" fn main() -> usize {
         println!("NOR flash: {:x}/{:x}{:x}", id[0], id[1], id[2],);
 
         println!("Load... 💾");
-        // println!("oreboot");
         let skip = 0x1 << 15; // 32K, the size of boot0
-        let size = ORE_SIZE >> 2;
-        load(skip, ORE_ADDR, size, &mut flash);
-
-        // println!("LinuxBoot");
-        // 32K + oreboot + dtfs + 4K, see oreboot dtfs
-        let skip = skip + ORE_SIZE + DTF_SIZE + 0x1000;
-        let size = (LIN_SIZE) >> 2;
-        load(skip, LIN_ADDR, size, &mut flash);
-
-        // println!("dtb");
-        // 32K + oreboot + dtfs + 4K + kernel
-        let skip = skip + LIN_SIZE;
-        let size = (DTB_SIZE) >> 2;
-        load(skip, DTB_ADDR, size, &mut flash);
+        load(skip, PAYLOAD_ADDR, PAYLOAD_SIZE, &mut flash);
 
         let _ = flash.free().free();
     }
@@ -329,33 +286,8 @@ extern "C" fn main() -> usize {
         let _ = flash.free().free();
     }
 
-    // debug helpers
-    /*
-    dump(LIN_ADDR + 0x00186f00);
-    dump(LIN_ADDR + 0x00186f04);
-    dump(LIN_ADDR + 0x00186f08);
-    dump(LIN_ADDR + 0x00286f00);
-    dump(LIN_ADDR + 0x00286f04);
-    dump(LIN_ADDR + 0x00286f08);
-    dump(LIN_ADDR + 0x00386f00);
-    dump(LIN_ADDR + 0x00386f04);
-    dump(LIN_ADDR + 0x00386f08);
-    dump(LIN_ADDR + 0x00486f00);
-    dump(LIN_ADDR + 0x00486f04);
-    dump(LIN_ADDR + 0x00486f08);
-
-    // println!("final checks - are those right?");
-    // oreboot first instruction
-    check_val(ORE_ADDR, addr_to_exp(ORE_ADDR));
-    // Linux first instruction
-    check_val(LIN_ADDR, addr_to_exp(LIN_ADDR));
-    // dtb begin
-    check_val(DTB_ADDR, addr_to_exp(DTB_ADDR));
-    // dtb end
-    check_val(DTB_END1, addr_to_exp(DTB_END1));
-    check_val(DTB_END2, addr_to_exp(DTB_END2));
-    check_val(DTB_END3, addr_to_exp(DTB_END3));
-    */
+    // debug helper
+    // dump(PAYLOAD_ADDR + 0x0000);
 
     for _ in 0..1000_0000 {
         core::hint::spin_loop();
